856bed87f5266071ef7ad8079f258d2b986a6ed0
srikanthpragada <srikanthpragada@gmail.com>
2025-08-14 19:57:54 +0530

    14-AUG

---
+++ demo/discount.py
@@ -0,0 +1,10 @@
+# Take price and display discount @10%
+
+price = float( input("Enter price :") )
+discount = price * 10 / 100
+print('Discount:', discount)
+
+
+
+
+
---
+++ demo/hello.py
@@ -0,0 +1,6 @@
+# Take name from user and say hello
+
+name = input("Enter your name :")
+print('Hello', name)
+
+
