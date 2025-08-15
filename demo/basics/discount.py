f9b948a8ee477adcc82fea29042ab841abdbc517
srikanthpragada <srikanthpragada@gmail.com>
2025-08-15 18:56:32 +0530

    15-AUG

--- demo/basics/discount.py
+++ demo/basics/discount.py
@@ -1,16 +1,9 @@
 # Take price and display discount @10%
 
-price = float( input("Enter price :") )
+price = float(input("Enter price :"))
 discount = price * 10 / 100
 net_price = price - discount
 
 print(f'Price    : {price:8.2f}')
 print(f'Discount : {discount:8.2f}')
 print(f'Net Price: {net_price:8.2f}')
-
-
-
-
-
-
-
